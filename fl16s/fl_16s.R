6dc8232475f4f023be431adc3933a4c019ef5366
NicolaKriefall <thenicolakriefall@gmail.com>
2020-04-17 12:28:19 -0400

    assigned taxonomy!

---
+++ fl16s/fl_16s.R
@@ -0,0 +1,322 @@
+#Nicola's FL Irma analysis - 16S
+#Almost entirely based on DADA2 Pipeline 1.8 Walkthrough:
+#https://benjjneb.github.io/dada2/tutorial.html
+#with edits by Carly D. Kenkel and modifications for my data by Nicola Kriefall
+#3/19/20
+
+#this looks promising:
+#https://github.com/RichieJu520/Co-occurrence_Network_Analysis
+
+#~########################~#
+##### PRE-PROCESSING #######
+#~########################~#
+
+#fastq files should have R1 & R2 designations for PE reads
+#Also - some pre-trimming. Retain only PE reads that match amplicon primer. Remove reads containing Illumina sequencing adapters
+
+#in Terminal home directory:
+#following instructions of installing BBtools from https://jgi.doe.gov/data-and-tools/bbtools/bb-tools-user-guide/installation-guide/
+#1. download BBMap package, sftp to installation directory
+#2. untar: 
+#tar -xvzf BBMap_(version).tar.gz
+#3. test package:
+#cd bbmap
+#~/bin/bbmap/stats.sh in=~/bin/bbmap/resources/phix174_ill.ref.fa.gz
+
+# my adaptors for 16S, which I saved as "adaptors.fasta"
+# >forward
+# AATGATACGGCGACCAC
+# >forwardrc
+# GTGGTCGCCGTATCATT
+# >reverse
+# CAAGCAGAAGACGGCATAC
+# >reverserc
+# GTATGCCGTCTTCTGCTTG
+
+#primers for 16S: 
+# >forward
+# GTGYCAGCMGCCGCGGTA
+# >reverse
+# GGACTACHVGGGTWTCTAAT
+
+##Still in terminal - making a sample list based on the first phrase before the underscore in the .fastq name
+#ls *R1_001.fastq | cut -d '_' -f 1 > samples.list
+#ls *R1_001.fastq | cut -d '_' -f 2 > samples.list
+
+##cuts off the extra words in the .fastq files
+#for file in $(cat samples.list); do  mv ${file}_*R1*.fastq ${file}_R1.fastq; mv ${file}_*R2*.fastq ${file}_R2.fastq; done 
+#for file in $(cat samples.list); do  mv *${file}_*R1*.fastq ${file}_R1.fastq; mv *${file}_*R2*.fastq ${file}_R2.fastq; done 
+
+##gets rid of reads that still have the adaptor sequence, shouldn't be there, I didn't have any
+#for file in $(cat samples.list); do ~/bin/bbmap/bbduk.sh in1=${file}_R1.fastq in2=${file}_R2.fastq ref=adaptors.fasta out1=${file}_R1_NoIll.fastq out2=${file}_R2_NoIll.fastq; done &>bbduk_NoIll.log
+
+##getting rid of first 4 bases (degenerate primers created them)
+#for file in $(cat samples.list); do ~/bin/bbmap/bbduk.sh in1=${file}_R1_NoIll.fastq in2=${file}_R2_NoIll.fastq ftl=4 out1=${file}_R1_NoIll_No4N.fastq out2=${file}_R2_NoIll_No4N.fastq; done &>bbduk_No4N.log
+
+##only keeping reads that start with the 16S primer
+#for file in $(cat samples.list); do ~/bin/bbmap/bbduk.sh in1=${file}_R1_NoIll_No4N.fastq in2=${file}_R2_NoIll_No4N.fastq restrictleft=20 k=10 literal=GTGYCAGCMGCCGCGGTA,GGACTACHVGGGTWTCTAAT copyundefined=t outm1=${file}_R1_NoIll_No4N_16S.fastq outu1=${file}_R1_check.fastq outm2=${file}_R2_NoIll_No4N_16S.fastq outu2=${file}_R2_check.fastq; done &>bbduk_16S.log
+##higher k = more reads removed, but can't surpass k=20 or 21
+
+##using cutadapt to remove primer
+# for file in $(cat samples.list)
+# do
+# cutadapt -g GTGYCAGCMGCCGCGGTA -a ATTAGAWACCCVHGTAGTCC -G GGACTACHVGGGTWTCTAAT -A TACCGCGGCKGCTGRCAC -n 2 --discard-untrimmed -o ${file}_R1.fastq -p ${file}_R2.fastq ${file}_R1_NoIll_No4N_16S.fastq ${file}_R2_NoIll_No4N_16S.fastq
+# done &> clip.log
+##-g regular 5' forward primer 
+##-G regular 5' reverse primer
+##-o forward out
+##-p reverse out
+##-max-n 0 means 0 Ns allowed
+##this overwrote my original renamed files 
+
+# did sftp of *_R1.fastq & *_R2.fastq files to the folder to be used in dada2
+
+#~########################~#
+##### DADA2 BEGINS #########
+#~########################~#
+
+#installing/loading packages:
+#if (!requireNamespace("BiocManager", quietly = TRUE))
+#  install.packages("BiocManager")
+#BiocManager::install("dada2", version = "3.8")
+library(dada2); packageVersion("dada2")
+#I have version 1.10.0 - tutorial says 1.8 but I think that's OK, can't find a version 1.10 walkthrough
+library(ShortRead)
+#packageVersion("ShortRead")
+library(Biostrings)
+#packageVersion("Biostrings")
+path <- "/Volumes/TOSHIBA EXT/flirma_apr2020/files" # CHANGE ME to the directory containing the fastq files after unzipping.
+
+fnFs <- sort(list.files(path, pattern = "_R1.fastq", full.names = TRUE))
+fnRs <- sort(list.files(path, pattern = "_R2.fastq", full.names = TRUE))
+
+get.sample.name <- function(fname) strsplit(basename(fname), "_")[[1]][1]
+sample.names <- unname(sapply(fnFs, get.sample.name))
+head(sample.names)
+sample.names
+
+#### check for primers ####
+FWD <- "GTGYCAGCMGCCGCGGTA"  ## CHANGE ME to your forward primer sequence
+REV <- "GGACTACHVGGGTWTCTAAT"  ## CHANGE ME...
+
+allOrients <- function(primer) {
+  # Create all orientations of the input sequence
+  require(Biostrings)
+  dna <- DNAString(primer)  # The Biostrings works w/ DNAString objects rather than character vectors
+  orients <- c(Forward = dna, Complement = complement(dna), Reverse = reverse(dna), 
+               RevComp = reverseComplement(dna))
+  return(sapply(orients, toString))  # Convert back to character vector
+}
+FWD.orients <- allOrients(FWD)
+REV.orients <- allOrients(REV)
+FWD.orients
+REV.orients
+
+fnFs.filtN <- file.path(path, "filtN", basename(fnFs)) # Put N-filterd files in filtN/ subdirectory
+fnRs.filtN <- file.path(path, "filtN", basename(fnRs))
+filterAndTrim(fnFs, fnFs.filtN, fnRs, fnRs.filtN, maxN = 0, multithread = TRUE)
+
+primerHits <- function(primer, fn) {
+  # Counts number of reads in which the primer is found
+  nhits <- vcountPattern(primer, sread(readFastq(fn)), fixed = FALSE)
+  return(sum(nhits > 0))
+}
+rbind(FWD.ForwardReads = sapply(FWD.orients, primerHits, fn = fnFs.filtN[[1]]), 
+      FWD.ReverseReads = sapply(FWD.orients, primerHits, fn = fnRs.filtN[[1]]), 
+      REV.ForwardReads = sapply(REV.orients, primerHits, fn = fnFs.filtN[[1]]),
+      REV.ReverseReads = sapply(REV.orients, primerHits, fn = fnRs.filtN[[1]]))
+#no primers - amazing
+
+#### Visualizing raw data ####
+
+#First, lets look at quality profile of R1 reads
+plotQualityProfile(fnFs.filtN[c(1,2,3,4)])
+plotQualityProfile(fnFs.filtN[c(362,363,364,365)])
+#looks mostly good up to 200
+
+#Then look at quality profile of R2 reads
+plotQualityProfile(fnRs.filtN[c(1,2,3,4)])
+plotQualityProfile(fnRs.filtN[c(362,363,364,365)])
+#180
+
+# Make directory and filenames for the filtered fastqs
+filt_path <- file.path(path, "trimmed")
+if(!file_test("-d", filt_path)) dir.create(filt_path)
+filtFs <- file.path(filt_path, paste0(sample.names, "_F_filt.fastq.gz"))
+filtRs <- file.path(filt_path, paste0(sample.names, "_R_filt.fastq.gz"))
+
+#changing a bit from default settings - maxEE=1 (1 max expected error, more conservative), truncating length at 200 bp for both forward & reverse [leaves ~50bp overlap], added "trimleft" to cut off primers [18 for forward, 20 for reverse]
+out <- filterAndTrim(fnFs, filtFs, fnRs, filtRs, 
+                     truncLen=c(200,180), #leaves ~50bp overlap
+                     maxN=0, #DADA does not allow Ns
+                     maxEE=c(1,1), #allow 1 expected errors, where EE = sum(10^(-Q/10)); more conservative, model converges
+                     truncQ=2, 
+                     #trimLeft=c(18,20), #N nucleotides to remove from the start of each read
+                     rm.phix=TRUE, #remove reads matching phiX genome
+                     matchIDs=TRUE, #enforce matching between id-line sequence identifiers of F and R reads
+                     compress=TRUE, multithread=TRUE,verbose=TRUE) # On Windows set multithread=FALSE
+
+head(out)
+tail(out)
+
+#~############################~#
+##### Learn Error Rates ########
+#~############################~#
+
+#setDadaOpt(MAX_CONSIST=30) #increase number of cycles to allow convergence
+errF <- learnErrors(filtFs, multithread=TRUE)
+errR <- learnErrors(filtRs, multithread=TRUE)
+
+#sanity check: visualize estimated error rates
+#error rates should decline with increasing qual score
+#red line is based on definition of quality score alone
+#black line is estimated error rate after convergence
+#dots are observed error rate for each quality score
+
+plotErrors(errF, nominalQ=TRUE) 
+plotErrors(errR, nominalQ=TRUE) 
+
+#~############################~#
+##### Dereplicate reads ########
+#~############################~#
+#Dereplication combines all identical sequencing reads into into “unique sequences” with a corresponding “abundance”: the number of reads with that unique sequence. 
+#Dereplication substantially reduces computation time by eliminating redundant comparisons.
+#DADA2 retains a summary of the quality information associated with each unique sequence. The consensus quality profile of a unique sequence is the average of the positional qualities from the dereplicated reads. These quality profiles inform the error model of the subsequent denoising step, significantly increasing DADA2’s accuracy.
+derepFs <- derepFastq(filtFs, verbose=TRUE)
+derepRs <- derepFastq(filtRs, verbose=TRUE)
+# Name the derep-class objects by the sample names
+names(derepFs) <- sample.names
+names(derepRs) <- sample.names
+
+#~###############################~#
+##### Infer Sequence Variants #####
+#~###############################~#
+
+dadaFs <- dada(derepFs, err=errF, multithread=TRUE)
+dadaRs <- dada(derepRs, err=errR, multithread=TRUE)
+
+#now, look at the dada class objects by sample
+#will tell how many 'real' variants in unique input seqs
+#By default, the dada function processes each sample independently, but pooled processing is available with pool=TRUE and that may give better results for low sampling depths at the cost of increased computation time. See our discussion about pooling samples for sample inference. 
+dadaFs[[1]]
+dadaRs[[1]]
+
+#~############################~#
+##### Merge paired reads #######
+#~############################~#
+
+#To further cull spurious sequence variants
+#Merge the denoised forward and reverse reads
+#Paired reads that do not exactly overlap are removed
+
+mergers <- mergePairs(dadaFs, derepFs, dadaRs, derepRs, verbose=TRUE)
+# Inspect the merger data.frame from the first sample
+head(mergers[[1]])
+
+summary((mergers[[1]]))
+
+#We now have a data.frame for each sample with the merged $sequence, its $abundance, and the indices of the merged $forward and $reverse denoised sequences. Paired reads that did not exactly overlap were removed by mergePairs.
+
+#~##################################~#
+##### Construct sequence table #######
+#~##################################~#
+#a higher-resolution version of the “OTU table” produced by classical methods
+
+seqtab <- makeSequenceTable(mergers)
+dim(seqtab)
+
+# Inspect distribution of sequence lengths
+table(nchar(getSequences(seqtab)))
+
+plot(table(nchar(getSequences(seqtab)))) #real variants appear to be right in that 244-264 window
+
+#The sequence table is a matrix with rows corresponding to (and named by) the samples, and 
+#columns corresponding to (and named by) the sequence variants. 
+#Sequences that are much longer or shorter than expected may be the result of non-specific priming, and may be worth removing
+
+seqtab2 <- seqtab[,nchar(colnames(seqtab)) %in% seq(200,300)] #again, being fairly conservative wrt length
+
+#~############################~#
+##### Remove chimeras ##########
+#~############################~#
+
+seqtab.nochim <- removeBimeraDenovo(seqtab2, method="consensus", multithread=TRUE, verbose=TRUE)
+dim(seqtab.nochim)
+#Identified 1375 bimeras out of 80386 input sequences.
+
+sum(seqtab.nochim)/sum(seqtab)
+#0.9963629
+#The fraction of chimeras varies based on factors including experimental procedures and sample complexity, 
+#but can be substantial. 
+
+#save
+saveRDS(seqtab.nochim, file="flirma_seqtab.nochim.rds")
+write.csv(seqtab.nochim, file="flirma_seqtab.nochim.csv")
+
+seqtab.nochim <- readRDS("flirma_seqtab.nochim.rds")
+
+#~############################~#
+##### Track Read Stats #########
+#~############################~#
+
+#### need to do this :(, I done goofed #### 
+
+getN <- function(x) sum(getUniques(x))
+track <- cbind(out, sapply(dadaFs, getN), sapply(mergers, getN), rowSums(seqtab), rowSums(seqtab.nochim))
+colnames(track) <- c("input", "filtered", "denoised", "merged", "tabled", "nonchim")
+rownames(track) <- sample.names
+head(track)
+tail(track)
+
+write.csv(track,file="mr16s_readstats.csv",row.names=TRUE,quote=FALSE)
+
+#~############################~#
+##### Assign Taxonomy ##########
+#~############################~#
+
+library(dada2)
+
+# #Using package DECIPHER as an alternatie to 'assignTaxonomy'
+# 
+# #BiocManager::install("DECIPHER")
+# library(DECIPHER); packageVersion("DECIPHER")
+# #citation("DECIPHER")
+# 
+# #http://DECIPHER.codes/Downloads.html. Download the SILVA SSU r132 (modified) file to follow along.
+# 
+# dna <- DNAStringSet(getSequences(seqtab.nochim)) # Create a DNAStringSet from the ASVs
+# load("~/Downloads/SILVA_SSU_r132_March2018.RData") # CHANGE TO THE PATH OF YOUR TRAINING SET
+# ids <- IdTaxa(dna, trainingSet, strand="top", processors=NULL, verbose=FALSE, threshold=50) # use all processors
+# ranks <- c("domain", "phylum", "class", "order", "family", "genus", "species") # ranks of interest
+# # Convert the output object of class "Taxa" to a matrix analogous to the output from assignTaxonomy
+# taxid <- t(sapply(ids, function(x) {
+#   m <- match(ranks, x$rank)
+#   taxa <- x$taxon[m]
+#   taxa[startsWith(taxa, "unclassified_")] <- NA
+#   taxa
+# }))
+# colnames(taxid) <- ranks; rownames(taxid) <- getSequences(seqtab.nochim)
+
+#also doing other taxonomy method:
+#Assign Taxonomy
+taxa <- assignTaxonomy(seqtab.nochim, "~/Downloads/silva_nr_v132_train_set.fa.gz",tryRC=TRUE)
+unname(head(taxa))
+taxa.plus <- addSpecies(taxa, "~/Downloads/silva_species_assignment_v132.fa.gz",tryRC=TRUE,verbose=TRUE)
+#507 out of 79011 were assigned to the species level.
+#Of which 436 had genera consistent with the input table.
+
+setwd("~/florida_irma")                                                                    
+saveRDS(taxa.plus, file="mr16s_taxaplus.rds")
+saveRDS(taxa, file="mr16s_taxa.rds")
+write.csv(taxa.plus, file="mr16s_taxaplus.csv")
+write.csv(taxa, file="mr16s_taxa.csv")
+
+saveRDS(seqtab.nochim, file="mr16s_seqtab.nochim.rds")
+write.csv(seqtab.nochim, file="mr16s_seqtab.nochim.csv")
+write.csv(seqtab.nochim, file="mr16s_seqtab.nochim_renamed.csv")
+
+#### Read in previously saved datafiles ####
+setwd("~/moorea_holobiont/mr_16S/")
+seqtab.nochim <- readRDS("mr16s_seqtab.nochim.rds")
+taxa <- readRDS("mr16s_taxa.rds")
+taxa.plus <- readRDS("mr16s_taxaplus.rds")
